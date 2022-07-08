45c84203423d9fcf9ca87cd4921969c0c8621ced
DJDeezy <diegojimenez1023@yahoo.com>
2022-07-08 13:50:46 -0700

    fixed table graphic

--- js/home.js
+++ js/home.js
@@ -21,17 +21,20 @@ var homeInformation = {
 
 var createHomePage = function() {
   changeTitle('Home');
-  var $mainContainer = $('#main-container');
-  var $profileContainer = $('<div class="col-12 col-m-12 profile-container">').appendTo($mainContainer);
   setCurrentPage('home');
+  var $mainContainer = $('#main-container');
 
   // Table graphic
-  var $tableContainer = $('<div class="col-12 col-m-12" id="table-container">').appendTo($($profileContainer));
+  // var $rotationContainer = $('<div class="col-12 col-m-12" id="rotation-container">').appendTo($($mainContainer));
+  var $rotationContainer = $('<div class="col-12 col-m-12" id="rotation-container">').prependTo($('body'));
+  // var $tableContainer = $('<div class="col-12 col-m-12" id="table-container">').appendTo($($profileContainer));
+  var $tableContainer = $('<div class="col-12 col-m-12" id="table-container">').appendTo($($rotationContainer));
   var $tableGlass = $('<div class="col-12 col-m-12" id="table-glass">').appendTo($($tableContainer));
   var $tableFrameTop = $('<div class="col-12 col-m-12" id="table-frame-top">').appendTo($($tableContainer));
   var $tableFrameFront = $('<div class="col-12 col-m-12" id="table-frame-front">').appendTo($($tableContainer));
 
   // Main photo
+  var $profileContainer = $('<div class="col-12 col-m-12 profile-container">').appendTo($mainContainer);
   var $profileImageContainer = $('<div class="col-12 col-m-12 padded-photo-container" id="profile-image-container">').appendTo($profileContainer);
   var $profileImage = $('<img class="photo", id="profile-image" src="./images/bartender.jpg">').appendTo($profileImageContainer);
   var $profileImageShadow = $('<div class="col-12 col-m-12" id="profile-image-shadow">').appendTo($profileContainer);
